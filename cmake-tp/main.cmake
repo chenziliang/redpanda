434909f917f037dc7b8c74d2ab49e1cede3ef3c9
Ken Chen <zlchen.ken@gmail.com>
2021-08-06 07:39:26 +0800

    tp fixes

---
+++ cmake-tp/main.cmake
@@ -0,0 +1,21 @@
+include(diagnostic_colors)
+
+set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/lib)
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/lib)
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/bin)
+set(TP_BUILD ON)
+
+# Alias libraries to minimize the cmake file changes
+add_library(Base64::base64 ALIAS base64)
+add_library(LZ4::LZ4 ALIAS lz4)
+add_library(Snappy::snappy ALIAS snappy)
+add_library(ZLIB::ZLIB ALIAS zlib)
+add_library(Roaring::roaring ALIAS roaring)
+add_library(Zstd::zstd ALIAS zstd)
+add_library(Boost::unit_test_framework ALIAS _boost_test)
+
+# add code
+include(testing)
+include(set_option)
+include(v_library)
+# add_subdirectory(src)
